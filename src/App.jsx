ec98283085f222ee632ffc12e223e6eea4081f82
Abhishek Panchal <abhishekpanchal8894@gmail.com>
2025-12-19 01:44:55 +0530

    create selected project component, and add logic to render it on click

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import NewProject from './components/NewProject';
 import ProjectsSideBar from './components/ProjectsSideBar';
 import NoProjectSelected from './components/NoProjectSelected';
+import SelectedProject from './components/SelectedProject';
 
 function App() {
   // null is sign for adding new project, undefined is for no project selected and not adding any projects
@@ -33,9 +34,18 @@ function App() {
     });
   }
 
-  console.log(projectsState);
+  function handleSelectProject(projectId) {
+    setProjectsState((prevState) => {
+      return { ...prevState, selectedProjectId: projectId };
+    });
+  }
 
-  let content;
+  const selectedProject = projectsState.projects.find(
+    (project) => projectsState.selectedProjectId === project.id
+  );
+
+  let content = <SelectedProject project={selectedProject}></SelectedProject>;
+
   if (projectsState.selectedProjectId === undefined)
     content = (
       <NoProjectSelected
@@ -54,6 +64,7 @@ function App() {
       <ProjectsSideBar
         projects={projectsState.projects}
         onCreateNewProject={handleStartAddProject}
+        onSelectProject={handleSelectProject}
       ></ProjectsSideBar>
 
       {content}
